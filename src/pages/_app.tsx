8952ab8c66b35848aae9e6b23bc268b690e2c0a5
om choudhary <choudharyom@hotmail.com>
2025-04-18 14:55:57 +0000

    fix tailwindcss issue

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,5 +2,9 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <div className="antialiased text-gray-800 bg-gray-50 min-h-screen">
+      <Component {...pageProps} />
+    </div>
+  );
 }
